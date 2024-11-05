08030d63467bcf7f0f7fdd6b1cb5464dd513e0b6
Moritz Mock <23076328+moritzmock@users.noreply.github.com>
2024-11-05 17:54:20 +0100

    fixed code

--- transformer/main.py
+++ transformer/main.py
@@ -1,17 +1,23 @@
+import argparse
+import os.path
+import shutil
 import torch
-from torch.utils.data import DataLoader
-from transformers import RobertaTokenizer, RobertaForSequenceClassification, Trainer, TrainingArguments
+from transformers import RobertaTokenizer, RobertaForSequenceClassification, Trainer, TrainingArguments, DataCollatorWithPadding
 from datasets import load_dataset, Dataset
 from evaluate import load
+from sklearn.metrics import confusion_matrix
+
 
 accuracy = load("accuracy")
 precision = load("precision")
 recall = load("recall")
 f1 = load("f1")
 
+
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
     predictions = torch.argmax(logits, dim=-1)
+
     return {
         "accuracy": accuracy.compute(predictions=predictions, references=labels)["accuracy"],
         "precision": precision.compute(predictions=predictions, references=labels, average="weighted")["precision"],
@@ -24,7 +30,71 @@ def tokenize(batch):
     return tokenizer(batch["text"], padding=True, truncation=True, max_length=128)
 
 
+def modify_data(data):
+    data = Dataset.from_dict({"text": data["combo"], "labels": data["labels"]})
+    data = data.map(tokenize, batched=True)
+    data.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
+    data = data.remove_columns(['text'])
+
+    return data
+
+
+def str2bool(value):
+    if isinstance(value, bool):
+        return value
+    if value.lower() in {'true', 'yes', 'y', '1'}:
+        return True
+    elif value.lower() in {'false', 'no', 'n', '0'}:
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Boolean value expected.")
+
+
+def read_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--output-path", required=True)
+    parser.add_argument("--model", default="roberta-base")
+    parser.add_argument("--clear-output-path", default=True, type=str2bool)
+
+    return parser.parse_args()
+
+
+def create_path_structure(path, clear):
+    if not os.path.exists(path):
+        path_parts = path.split("/")
+        current_path = ""
+
+        for part in path_parts:
+            current_path = os.path.join(current_path, part)
+            if not os.path.exists(current_path) and current_path != "":
+                os.makedirs(current_path)
+                print(f"Directory created: {current_path}")
+            else:
+                print(f"Directory already exists: {current_path}")
+            if current_path == "":
+                current_path = "/"
+
+        print("Path structure created successfully.")
+
+    if clear is True:
+        for item in os.listdir(path):
+            item_path = os.path.join(path, item)
+
+            if os.path.isfile(item_path) or os.path.islink(item_path):
+                os.remove(item_path)
+                print(f"File removed: {item_path}")
+            elif os.path.isdir(item_path):
+                shutil.rmtree(item_path)  # Remove directory and all its contents
+                print(f"Directory removed: {item_path}")
+
+        print(f"All objects in {path} have been removed.")
+
+
 if __name__ == "__main__":
+    args = read_args()
+    print(args)
+
     langs = ['java', 'python', 'pharo']
     labels = {
         'java': ['summary', 'Ownership', 'Expand', 'usage', 'Pointer', 'deprecation', 'rational'],
@@ -33,47 +103,57 @@ if __name__ == "__main__":
                   'Collaborators']
     }
 
-    lan = langs[0]
-
     ds = load_dataset('NLBSE/nlbse25-code-comment-classification')
     print("Dataset was loaded successfully!")
 
-    train = ds[f"{lan}_train"]
-    model = RobertaForSequenceClassification.from_pretrained("roberta-base", num_labels=len(labels[lan]))
-    model.config.problem_type = "multi_label_classification"
+    for lan in langs:
+        create_path_structure(f"{args.output_path}/{lan}/results", args.clear_output_path)
+        create_path_structure(f"{args.output_path}/{lan}/log", args.clear_output_path)
+        create_path_structure(f"{args.output_path}/{lan}/models", args.clear_output_path)
 
+        train = ds[f"{lan}_train"]
+        test = ds[f"{lan}_test"]
 
-    print("Model was loaded successfully!")
+        model = RobertaForSequenceClassification.from_pretrained(args.model, num_labels=len(labels[lan]))
+        model.config.problem_type = "multi_label_classification"
 
-    tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
+        print("Model was loaded successfully!")
 
-    print("Tokenizer was loaded successfully!")
+        tokenizer = RobertaTokenizer.from_pretrained(args.model)
 
-    train_data = Dataset.from_dict({"text": train["combo"], "labels": train["labels"]})\
-        .map(tokenize, batched=True)\
-        .set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
+        print("Tokenizer was loaded successfully!")
 
-    print("Dataset was mutated successfully!")
+        train_data = modify_data(train)
+        test_data = modify_data(test)
 
-    training_args = TrainingArguments(
-        output_dir="./results",
-        eval_strategy="no",
-        per_device_train_batch_size=8,
-        per_device_eval_batch_size=8,
-        num_train_epochs=3,
-        weight_decay=0.01,
-        logging_dir="./logs",
-    )
+        print("Dataset was mutated successfully!")
 
-    print("TrainingArguments were created successfully!")
+        training_args = TrainingArguments(
+            output_dir=f"{args.output_path}/{lan}/results",
+            eval_strategy="no",
+            per_device_train_batch_size=8,
+            per_device_eval_batch_size=8,
+            num_train_epochs=3,
+            weight_decay=0.01,
+            logging_dir=f"{args.output_path}/{lan}/logs"
+        )
 
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=train,
-        compute_metrics=compute_metrics,
-    )
+        print("TrainingArguments were created successfully!")
 
-    print("Trainer was created successfully!")
+        data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
-    trainer.train()
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=train_data,
+            compute_metrics=compute_metrics,
+            data_collator=data_collator
+        )
+
+        print("Trainer was created successfully!")
+
+        trainer.train()
+
+        trainer.save_model(f"{args.output_path}/{lan}/models")
+
+        trainer.evaluate(eval_dataset=test_data)
