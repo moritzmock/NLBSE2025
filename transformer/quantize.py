8ffd0ca01430c4ba09115fd59f975e19ee86f7ae
Moritz Mock <23076328+moritzmock@users.noreply.github.com>
2024-12-06 14:17:40 +0100

    added quantization

---
+++ transformer/quantize.py
@@ -0,0 +1,49 @@
+import argparse
+
+from transformers import RobertaForSequenceClassification
+from transformers import BitsAndBytesConfig
+import numpy as np
+import random
+import torch
+
+def set_seed(seed):
+    """for reproducibility
+    :param seed:
+    :return:
+    """
+    np.random.seed(seed)
+    random.seed(seed)
+
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
+    torch.backends.cudnn.enabled = True
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+
+def read_args():
+
+    args = argparse.ArgumentParser()
+
+    args.add_argument("--input-path", required=True)
+
+    return args.parse_args()
+
+
+if __name__ == "__main__":
+    set_seed(42)
+
+    args = read_args()
+
+    nf4_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_compute_dtype=torch.bfloat16
+    )
+
+    model = RobertaForSequenceClassification.from_pretrained(args.input_path, load_in_4bit=True, device_map="auto")
+
